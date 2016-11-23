9f7a455892c0393a994aa7cb7531de3606412329
YoannLTH <yoann.lathuiliere@gmail.com>
2016-11-23 15:11:32 +0100

    Finish UI Design

---
+++ PartyRockApp/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by yoann lathuiliere on 23/11/2016.
+//  Copyright © 2016 ylth. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
---
+++ PartyRockApp/VideoVC.swift
@@ -0,0 +1,30 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by yoann lathuiliere on 23/11/2016.
+//  Copyright © 2016 ylth. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
